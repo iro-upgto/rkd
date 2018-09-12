bc6800ca275dc83ca38a65d9f038400844bb82dc
JorgeDeLosSantos <delossantosmfq@gmail.com>
2018-09-12 12:15:48 -0500

    Adicionando módulos

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+# Current status: pre-alpha
+
+setup(name='nusa',
+      version='0.1.0',
+      description='Python library for kinematic analysis of robots',
+      author='IRO',
+      author_email='jdelossantos@upgto.edu.mx',
+      license = "MIT",
+      keywords=["Robotics","Kinematics"],
+      url='https://github.com/iro-upgto/rkd',
+      packages=['rkd'],
+      classifiers=[
+      "Development Status :: 2 - Pre-Alpha",
+      "Intended Audience :: Education",
+      "Intended Audience :: Developers",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+      "Programming Language :: Python",
+      "Programming Language :: Python :: 2.7",
+      "Programming Language :: Python :: Implementation :: CPython",
+      ]
+      )
