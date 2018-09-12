42e2aef5ddfd1a2bd51d67721486f5d081e21d58
JorgeDeLosSantos <delossantosmfq@gmail.com>
2018-09-12 16:21:56 -0500

    Actualizando

--- rkd/transformations.py
+++ rkd/transformations.py
@@ -1,4 +1,22 @@
 # Transformations
+import numpy as np
 
 
+def rotz(theta, deg=False):
+    """
+    Calculates the rotation matrix about the z-axis
 
+    *theta* : float or int
+        Rotation angle (given in radians by default)
+
+    *deg* : bool
+        ¿Is theta given in degrees?
+    """
+    if deg: # If theta is given in degrees -> convert to radians
+        theta = theta*np.pi/180
+    ct = np.cos(theta)
+    st = np.sin(theta)
+    R = np.array([[ct, -st, 0],
+                  [st, ct, 0],
+                  [0, 0, 1]])
+    return R
