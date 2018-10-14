f4aa13ea540cf825d0e3510aa2dc2f84f06e09c0
Jiovani Ledesma Arredondo <giioledesma@gmail.com>
2018-10-14 13:43:46 -0500

    Actualizado

--- rkd/kinematics.py
+++ rkd/kinematics.py
@@ -3,3 +3,8 @@ import numpy as np
 from rkd.abc import *
 from rkd.util import *
 from rkd.transformations import *
+
+def jacobiano(*matrices, type):
+    #1. Que el usuario ingrese todas las matrices
+    #2. Que el usuario ingrese los parametros DH en forma de arreglo y que la misma función se encargue de realizar las multiplicaciones de matrices correspondientes
+    return
