a7d67c5d5d83dd88d1ada26133dee0c171d4fd48
Jiovani Ledesma Arredondo <giioledesma@gmail.com>
2018-10-18 23:22:48 -0500

    Actualizado

--- rkd/kinematics.py
+++ rkd/kinematics.py
@@ -1,12 +1,15 @@
 #Kinematics
 import numpy as np
+from numpy import *
 from math import *
-from rkd.abc import *
 from rkd.util import *
 from rkd.transformations import *
 from rkd.mathematical_algorithms import *
 
-def jacobiano(*args):
+def jacobian(*args):
+    """
+    Calculate the Jacobian matrix to determine the differential kinematics of manipulators / robots
+    """
     Ts = []
     type = []
     for k in args:
@@ -31,6 +34,9 @@ def jacobiano(*args):
     return M_
 
 def z(i,Ts):
+    """
+    z-dir of every i-Frame wrt 0-Frame
+    """
     if i == 0: return np.array([0,0,1])
     MTH = np.eye(4)
     for k in range(i):
@@ -38,12 +44,54 @@ def z(i,Ts):
     return MTH[:3,2]
 
 def p(i,Ts):
+    """
+    Position for every i-Frame wrt 0-Frame
+    """
     if i == 0: return np.array([0,0,0])
     MTH = np.eye(4)
     for k in range(i):
         MTH = m_mult(MTH,Ts[k])
     return MTH[:3,3]
 
+#Kinematics inverse
+
+def kinematics_inv(J, b, x0, deg = False,eps = 1e-6):
+    """
+    Calculate the inverse kinematics from a numerical method
+
+    ** Solved from the numerical method 'Newton - Raphson' **
+
+    IMPORT: You must create your two functions of J (Jacobian), b (initial values ​​evaluated)
+    """
+    k = 0 #iterations
+    while True:
+        x = np.linalg.solve(J(x0), -b(x0))
+        if norm(x) < eps: break
+        x0 += x
+        k += 1 #Increase of iterations counter
+    if deg:
+        return rad2deg(x0), x, k
+    return x0, x, k
+
+def j1(x0):
+    """
+    Calculates the Jacobian matrix evaluated with the initial values
+    """
+    t1,t2 = x0
+    J = np.array([[-100*sin(t1) - 100*sin(t1 + t2), -100*sin(t1 + t2)],
+                  [100*cos(t1) + 100*cos(t1 + t2), 100*cos(t1 + t2)]])
+    return J
+
+def b1(x0):
+    """
+    calculates the aproximation of vector 'bi' on the method 'Newton - Rapshon'
+    """
+    t1,t2 = x0
+    F = np.array([100*cos(t1) + 100*cos(t1 + t2) - 100, 100*sin(t1) + 100*sin(t1 + t2) - 100])
+    return F
+
 if __name__=="__main__":
-    
-    print(jacobiano((100,0,0,pi/6,'r'), (100,0,0,pi/4,'r'), (20,0,0,-pi/6,'r')))
+
+    x0 = np.array([pi/6, pi/8])
+
+    print(kinematics_inv(j1, b1, x0, True))
