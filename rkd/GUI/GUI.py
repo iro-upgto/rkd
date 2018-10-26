f3a134909066768e0942f0c9f94f624c580bc62a
Jiovani Ledesma Arredondo <giioledesma@gmail.com>
2018-10-26 01:46:12 -0500

    Actualizado

--- rkd/GUI/GUI.py
+++ rkd/GUI/GUI.py
@@ -4,12 +4,13 @@ from tkinter import messagebox
 import matplotlib
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-from matplotlib.backends.backend_tkagg import (
-    FigureCanvasTkAgg, NavigationToolbar2Tk)
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 # Implement the default Matplotlib key bindings.
 from matplotlib.figure import Figure
-
 import numpy as np
+from rkd.transformations import *
+from rkd.kinematics import *
+global a
 
 class GUI(Tk):
 
@@ -27,9 +28,7 @@ class GUI(Tk):
         self.title("Robot Kinematics")
         img = PhotoImage(file = "img/robot.png")        
         self.call("wm", "iconphoto", self._w, img)
-        self.geometry("1200x650+300+10")
-        self.minsize(1150, 600)
-        self.maxsize(1200, 650)
+        self.geometry("1200x650+300+10")        
         barMenu = Menu(self)
         menuInfo = Menu(barMenu)
         menuhelp = Menu(barMenu)        
@@ -71,7 +70,7 @@ class main(Frame):
 
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
-        self.controller = controller        
+        self.controller = controller
         label = Label(self, text="Robot Kinematics", font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)        
         btn_transformations = Button(self, text = "Transformations", font = controller.Arial16, width = 40, height = 3, borderwidth = 5, cursor = "hand1", command = lambda: controller.show_frame("transformations"))
@@ -110,7 +109,7 @@ class forward_kinematics(Frame):
         btn_back = Button(self, text = "Back", font = controller.Arial14, width = 15, height = 3, borderwidth = 5, cursor = "hand1", command = lambda: controller.show_frame("main"))
         btn_back.pack()
         f = Figure(figsize = (5,5), dpi = 100)
-        a = f.add_subplot(111)
+        a = f.add_subplot(111, projection="3d")
         a.plot([1,2,3,4,5,6,7,8],[5,6,1,3,8,9,3,5])
 
         canvas = FigureCanvasTkAgg(f, self)  # A tk.DrawingArea.
@@ -128,51 +127,92 @@ class rotations(Frame):
 
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
-        self.controller = controller        
-        label = Label(self, text = "Rotations", font = controller.title_font)
-        label.pack(side = "top", fill = "x", pady = 5)
-        Label(self, text = "Axis:", font = controller.Arial14).pack(side = TOP, pady = 5)        
-        txt_axis = Entry(self, font = controller.Arial14)
-        txt_axis.pack(side = TOP, pady = 5)
-        Label(self, text = "Angle:", font = controller.Arial14).pack(side = TOP, pady = 5)
-        txt_angle = Entry(self, font = controller.Arial14)
-        txt_angle.pack(side = TOP, pady = 5)
-        btn_go = Button(self, text = "GO", font = controller.Arial14, width = 15, height = 2, borderwidth = 5, cursor = "hand1", command = lambda: self.go_pulse(txt_axis.get(), txt_angle.get()))
-        btn_go.pack(side = TOP, pady = 5)
-        btn_back = Button(self, text = "Back", font = controller.Arial14, width = 15, height = 2, borderwidth = 5, cursor = "hand1", command = lambda: controller.show_frame("transformations")) 
-        btn_back.pack(side = TOP, pady = 5)        
+        self.controller = controller
+        frame1 = Frame(self, width = 650, height = 500)
+        frame1.pack(side = "left", anchor = "n")
+        label = Label(frame1, text = "Rotations", font = controller.title_font)
+        label.pack(side = "top", fill = "x", padx = 5, pady = 10)
+        Label(frame1, text = "", font = controller.Arial14).pack(side = TOP, padx = 5, pady = 10)
+        Label(frame1, text = "Axis:", font = controller.Arial14).pack(side = TOP, padx = 5, pady = 2)        
+        txt_axis = Entry(frame1, font = controller.Arial14)
+        txt_axis.pack(side = TOP, padx = 5, pady = 2)
+        Label(frame1, text = "", font = controller.Arial14).pack(side = TOP, padx = 5, pady = 10)
+        Label(frame1, text = "Angle:", font = controller.Arial14).pack(side = TOP, padx = 5, pady = 2)
+        txt_angle = Entry(frame1, font = controller.Arial14)
+        txt_angle.pack(side = TOP, padx = 5, pady = 2)
+        Label(frame1, text = "", font = controller.Arial14).pack(side = TOP, padx = 5, pady = 10)
+        f = Figure(figsize = (5,5), dpi = 100)
+        a = f.add_subplot(111, projection='3d')
+        a.mouse_init(self)     
+        a.clear()
+        canvas = FigureCanvasTkAgg(f, self)  # A tk.DrawingArea.
+        canvas.get_tk_widget().pack(side = BOTTOM, fill=BOTH, expand=True)
+        toolbar = NavigationToolbar2Tk(canvas, self)
+        toolbar.update()
+        canvas.get_tk_widget().pack(side = BOTTOM, fill=BOTH, expand=True)
+        btn_go = Button(frame1, text = "GO", font = controller.Arial14, width = 15, height = 2, borderwidth = 5, cursor = "hand1", command = lambda: self.go_pulse(txt_axis.get(), txt_angle.get(), a, canvas))
+        btn_go.pack(side = TOP, padx = 5, pady = 10)
+        btn_back = Button(frame1, text = "Back", font = controller.Arial14, width = 15, height = 2, borderwidth = 5, cursor = "hand1", command = lambda: controller.show_frame("transformations")) 
+        btn_back.pack(side = TOP, padx = 5, pady = 10)
+        
 
     #Functions for buttons
     
-    def go_pulse(self, axis, angle):        
+    def go_pulse(self, axis, angle, ax, canvas):
         if ((axis == "") or (angle == "")):
             messagebox.showwarning("Warning", "Check the TextBox")
 
         if ((axis != "") or (angle != "")):
             answer = messagebox.askquestion("Important to answer", "Are you entering the angles in degrees?")
-            if ((answer == "sí") or (answer == "Sí") or (answer == "yes") or (answer == "Yes")):
-                f = Figure(figsize = (5,2), dpi = 100)
-                a = f.add_subplot(111, projection='3d')
-                self.draw_uvw(np.eye(4))
-                canvas = FigureCanvasTkAgg(f, self)  # A tk.DrawingArea.
+            if ((answer == "sí") or (answer == "Sí") or (answer == "yes") or (answer == "Yes")):             
+                a = ax
+                canvas = canvas
+                a.clear()
+                self.draw_uvw(np.eye(4), a)
+                if ((axis == "x") or (axis == "X")):                    
+                    rot_x = rotx(float(angle), True)                    
+                    self.draw_uvw_rot(rot_x, a)
+
                 canvas.draw()
-                canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=True)
+                
+            
+                
 
-                toolbar = NavigationToolbar2Tk(canvas, self)
-                toolbar.update()
-                canvas.get_tk_widget().pack(side= TOP, fill=BOTH, expand=True)                
-
-    def draw_uvw(self, T):
-        O=T[:3,3]
+    def draw_uvw(self, T, ax):
+        O=(0,0,0)
         U=T[:3,0]
         V=T[:3,1]
         W=T[:3,2]        
-        plt.quiver(float(O[0]),float(O[1]),float(O[2]),float(U[0]),float(U[1]),float(U[2]),color="yellow") # Eje u
-        plt.quiver(float(O[0]),float(O[1]),float(O[2]),float(V[0]),float(V[1]),float(V[2]),color="blue") # Eje v
-        plt.quiver(float(O[0]),float(O[1]),float(O[2]),float(W[0]),float(W[1]),float(W[2]),color="white") # Eje w
+        ax.quiver(float(O[0]),float(O[1]),float(O[2]),float(U[0]),float(U[1]),float(U[2]),color="red", alpha = 0.4, label = "Xo") # Eje u
+        ax.quiver(float(O[0]),float(O[1]),float(O[2]),float(V[0]),float(V[1]),float(V[2]),color="green", alpha = 0.4, label = "Yo") # Eje v
+        ax.quiver(float(O[0]),float(O[1]),float(O[2]),float(W[0]),float(W[1]),float(W[2]),color="blue", alpha = 0.4, label = "Zo") # Eje w
+        ax.set_xlabel('X axis')
+        ax.set_ylabel('Y axis')
+        ax.set_zlabel('Z axis')
+        ax.set_xlim([-1,1])
+        ax.set_ylim([-1,1])
+        ax.set_zlim([-1,1])
+        ax.legend()
+        ax.set_aspect("equal")
         return
-                
 
+    def draw_uvw_rot(self, T, ax):
+        O=(0,0,0)
+        U=T[:3,0]
+        V=T[:3,1]
+        W=T[:3,2]        
+        ax.quiver(float(O[0]),float(O[1]),float(O[2]),float(U[0]),float(U[1]),float(U[2]),color="red", label = "Xr") # Eje u
+        ax.quiver(float(O[0]),float(O[1]),float(O[2]),float(V[0]),float(V[1]),float(V[2]),color="green", label = "Yr") # Eje v
+        ax.quiver(float(O[0]),float(O[1]),float(O[2]),float(W[0]),float(W[1]),float(W[2]),color="blue", label = "Zr") # Eje w
+        ax.set_xlabel('X axis')
+        ax.set_ylabel('Y axis')
+        ax.set_zlabel('Z axis')
+        ax.set_xlim([-1,1])
+        ax.set_ylim([-1,1])
+        ax.set_zlim([-1,1])
+        ax.legend()
+        ax.set_aspect("equal")
+        return
 
 if __name__ == "__main__":
     app = GUI()
